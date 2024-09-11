ff87764e0fb9deeeefcbcc1de27b614d70a27236
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-09-11 17:48:18 +0530

    movie app completed

--- imdb-fe/src/App.js
+++ imdb-fe/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Header from './Component/Header/Header';
 import Home from './Pages/Home/Home';
+import MovieList from './Component/movieList/MovieList';
+import Movie from './Pages/MovieDetails/Movie';
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
       <Header/>
         <Routes>
           <Route index element={<Home/>}></Route>
-          <Route path='/movie/:id' element={<h1>Movie Detail page</h1>}></Route>
-          <Route path='/movie/type' element={<h1>Movie List page</h1>}></Route>
+          <Route path='movie/:id' element={<Movie/>}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path='/*' element={<h1>Page Not Found</h1>}></Route>
         </Routes>
       </BrowserRouter>
--- imdb-fe/src/Pages/Home/Home.js
+++ imdb-fe/src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
+import MovieList from "../../Component/movieList/MovieList";
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -46,6 +47,7 @@ const Home = () => {
             ))}
         </Carousel>
       </div>
+      <MovieList/>
     </>
   );
 };
