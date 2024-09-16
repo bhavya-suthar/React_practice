dba22a14aca905ac50f0220e0b494190eda09e6d
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-09-16 17:34:33 +0530

    modify store

--- NewsApp(redux-thunk)/src/Redux/store.js
+++ NewsApp(redux-thunk)/src/Redux/store.js
@@ -1,6 +1,20 @@
-import { legacy_createStore as createStore , applyMiddleware} from "redux";
+// import { legacy_createStore as createStore , applyMiddleware} from "redux";
+// import newsReducer from './newsReducer'
+
+// export const store = createStore(newsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(thunk)
+// )
+
+
+
+import { legacy_createStore as createStore, applyMiddleware, compose } from "/node_modules/.vite/deps/redux.js?v=a47720fb";
+// import thunk from "/node_modules/.vite/deps/redux-thunk.js?v=a47720fb";
 import {thunk} from 'redux-thunk'
-import newsReducer from './newsReducer'
+import newsReducer from "/src/Redux/newsReducer.js";
 
-export const store = createStore(newsReducer,applyMiddleware(thunk))
+// Use compose to combine middleware and Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+export const store = createStore(
+  newsReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
