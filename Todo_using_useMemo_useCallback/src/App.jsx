fa1b58e53777f7d242b0ceaf7c9e27cd51e4fed3
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-11-27 17:50:33 +0530

    apply add and delete functionality

--- Todo_using_useMemo_useCallback/src/App.jsx
+++ Todo_using_useMemo_useCallback/src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { FaFilter } from "react-icons/fa";
+import {
+  FcAlphabeticalSortingAz,
+  FcAlphabeticalSortingZa,
+} from "react-icons/fc";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState();
+  // console.log("🚀 ~ App ~ input:", input);
+
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    // console.log(
+    //   "🚀 ~ const[todos,setTodos]=useState ~ savedTodos:",
+    //   savedTodos
+    // );
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+  // console.log("🚀 ~ App ~ todos:", todos);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  // const handleAddTodo = () => {
+  //   setTodos([...todos, {id:Date.now(), text: input }]);
+  //   setInput("");
+  // };
+
+  const add = useCallback(() => {
+    if (input.trim() == "") return;
+    setTodos([...todos, { id: Date.now(), text: input }]);
+    setInput("");
+  }, [input]);
+
+  // const handleDelete =(id)=>{
+  //   const filteredTodo = todos.filter((todo)=> todo.id !== id )
+  //   console.log("🚀 ~ handleDelete ~ filteredTodo:", filteredTodo)
+  //   localStorage.setItem("todos",JSON.stringify(filteredTodo))
+  // }
+
+  const handleDelete = useCallback(
+    (id) => {
+      const filteredTodo = todos.filter((todo) => todo.id !== id);
+      console.log("🚀 ~ handleDelete ~ filteredTodo:", filteredTodo);
+      setTodos(filteredTodo);
+      // localStorage.setItem("todos",JSON.stringify(filteredTodo))
+    },
+    [todos]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "40px",
+        }}
+      >
+        Todo List Using UseMemo And UseCallback
+      </h1>
+      <div style={{ display: "flex", gap: "10px", justifyContent: "center" }}>
+        <input
+          type="text"
+          style={{ height: "30px" }}
+          value={input}
+          onChange={handleChange}
+        />
+        <button
+          style={{
+            backgroundColor: "green",
+            border: "0",
+            height: "35px",
+            width: "55px",
+            borderRadius: "5px",
+          }}
+          onClick={add}
+        >
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div style={{display:"flex",gap:"10px"}}>
+          <FcAlphabeticalSortingAz style={{fontSize:"35px",backgroundColor:"darkred",borderRadius:"5px"}}/>
+          <FcAlphabeticalSortingZa style={{fontSize:"35px",backgroundColor:"darkred",borderRadius:"5px"}}/>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div style={{ marginLeft: "40%" }}>
+        {todos.length === 0 && <h1>No Todos Found</h1>}
+        {todos.map((e, index) => (
+          <div
+            style={{ display: "flex", alignItems: "center", gap: "5px" }}
+            key={index}
+          >
+            <h2>
+              {index + 1} {e.text}
+            </h2>
+            <button
+              style={{
+                height: "30px",
+                backgroundColor: "purple",
+                color: "white",
+                border: "0",
+                borderRadius: "3px",
+                padding: "10px",
+              }}
+              onClick={() => handleDelete(e.id)}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
