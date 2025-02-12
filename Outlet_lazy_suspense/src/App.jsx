4c659d6f9efef5540fb13db804b1102911367874
Bhavya Suthar <bhavyasuthar06@gmail.com>
2025-02-12 18:32:09 +0530

    lazy and suspense concept done

--- Outlet_lazy_suspense/src/App.jsx
+++ Outlet_lazy_suspense/src/App.jsx
@@ -1,9 +1,14 @@
 // import './App.css'
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(()=> import('./pages/Home'))
+const About = React.lazy(()=> import('./pages/About'))
+const Contact = React.lazy(()=> import('./pages/Contact'))
 
 function App() {
   const router = createBrowserRouter([
@@ -21,7 +26,9 @@ function App() {
   return (
     <>
       <h1>App</h1>
+      <Suspense fallback={<h1>Loading.....</h1>}>
       <RouterProvider router={router} />
+      </Suspense>
     </>
   );
 }
